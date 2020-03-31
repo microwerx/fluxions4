3afd3595b4465ab4e3bfcddb4ecb64262663be10
Jonathan Metzgar <microwerx@outlook.com>
2020-03-31 14:22:04 -0800

    add a color struct

---
+++ include/fluxions_gte_base.hpp
@@ -0,0 +1,14 @@
+#ifndef FLUXIONS_GTE_BASE_HPP
+#define FLUXIONS_GTE_BASE_HPP
+
+namespace Fluxions {
+	struct _Color4f {
+		float r = 0.0f;
+		float g = 0.0f;
+		float b = 0.0f;
+		float a = 0.0f;
+		_Color4f(float r = 0.0f, float g = 0.0f, float b = 0.0f, float a = 0.0f) :r(r), g(g), b(b), a(a) {}
+	};
+}
+
+#endif
