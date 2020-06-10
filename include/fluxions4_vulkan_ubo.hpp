24b1dc798fa9de5912ba8709037717da5c907d58
microwerx <microwerx@outlook.com>
2020-06-10 06:42:09 -0800

    add standard ubo object

---
+++ include/fluxions4_vulkan_ubo.hpp
@@ -0,0 +1,24 @@
+#ifndef FLUXIONS4_VULKAN_UBO_HPP
+#define FLUXIONS4_VULKAN_UBO_HPP
+
+namespace Fluxions {
+	struct VertShaderUbo {
+		FxMatrix4f modelview;
+		FxMatrix4f modelviewprojection;
+		float normal[12];
+		FxColor4f color{ 0.1f, 0.2f, 0.3f, 1.0f };
+	};
+
+	struct FragShaderUbo {
+		FxColor4f Kd{ 1.0f, 1.0f, 1.0f, 1.0f };
+		FxColor4f Ks{ 1.0f, 1.0f, 1.0f, 1.0f };
+		float spec_roughness;
+	};
+
+	struct StandardUbo {
+		VertShaderUbo vs_ubo;
+		FragShaderUbo fs_ubo;
+	};
+}
+
+#endif
